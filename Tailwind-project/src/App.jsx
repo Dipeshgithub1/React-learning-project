0ca36fde8054b15cd639e0c28e62a524f92dde1f
Dipesh Kumar <dipesh.kumar@iic.ac.in>
2024-12-19 23:05:21 +0530

    tailwindcss project added

---
+++ Tailwind-project/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <button type="button" class="bg-indigo-500 ..." disabled>
+  <svg class="motion-reduce:hidden animate-spin ..." viewBox="0 0 24 24"></svg>
+  Processing...
+</button>
+         
+  );
+}
+
+export default App;
+
+
+ 
+
+
+
+
