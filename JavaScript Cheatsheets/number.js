521345f5f4f59a28d7bc6464b4df537664632043
Dipesh Kumar <dipesh.kumar@iic.ac.in>
2024-12-31 23:00:16 +0530

    JS cheatsheets

---
+++ JavaScript Cheatsheets/number.js
@@ -0,0 +1,61 @@
+var num = 213;
+
+var num1 = 213.3456711;
+
+ // JS valueof() Method
+ console.log("Output valueOf : " + num.valueOf());
+
+ // JS tostring() Method
+ console.log("Output Tostring : " + num.toString(2));
+
+
+    // JS tofixed() Method
+    console.log("Output  tofixed: " + num1.toString(2));
+
+    // JS topricision() Method
+    console.log("Output precise : " + num1.toPrecision(3));
+
+    
+    // JS toexponential() Method
+    console.log("Output exp : " + num1.toExponential(4));
+
+
+    console.log(Math.LN10)
+
+
+
+ //Log
+console.log(Math.LN10)
+
+console.log(Math.LOG2E)
+
+console.log(Math.LOG10E)
+
+//SQRT
+console.log(Math.SQRT2)
+
+console.log(Math.SQRT1_2)
+
+//LN2
+console.log(Math.LN2)
+console.log(Math.E)
+//Round
+console.log(Math.round(5.8))
+
+console.log(Math.PI)
+
+//SINE
+console.log(Math.sin(90 * Math.PI / 180))
+
+console.log(Math.sin(45));
+
+//TAN
+console.log(Math.sin(60 * Math.PI / 180))
+
+//MAX
+console.log(Math.max(0,150,30,20,-8,-200));
+//MIN
+console.log(Math.min(0,150,30,20,-8,-200));
+//power
+console.log(Math.pow(3,4))
+
