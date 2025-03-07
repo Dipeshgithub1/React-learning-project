d91c665324d15e6c22d3d041eec20fdb040c126d
Dipesh Kumar <dipesh.kumar@iic.ac.in>
2025-03-07 21:10:34 +0530

    console method added

---
+++ JavaScript Cheatsheets/Console Method/consolelog.cpp
@@ -0,0 +1,17 @@
+//console.log - ouput a message to the console
+
+console.log('Hello world')
+
+//console.warn() - ouput for long warning messages
+console.log("This is a warning")
+//console.error() - logs an error message 
+console.error("this is an erroe")
+
+// console.info() - logs an informational message
+console.info("This is an info message")
+
+//console.Table() - used to represent objects or array in a table
+console.table([{name: 'alice',age:25},{name:'bob',age:30}])
+
+//console.clear() - used to clear the console
+console.clear()
